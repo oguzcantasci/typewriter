e1fec7106c792f7fef9cc68b8877c767e96122ee
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-12 01:03:42 -0500

    Prints a string with a typewriter-like animation

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let time = -50;
+
+for (const char of sentence) {
+  time += 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time
+  );
+}
+setTimeout(() => {
+  process.stdout.write(`\n`);
+}, time);
